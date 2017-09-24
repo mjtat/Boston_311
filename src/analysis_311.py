f78661043df0f5489e87453d616ff9a822fb197b
mjtat <michelle@michelletat.com>
2017-09-24 01:09:44 -0400

    dfgh

--- src/analysis_311.py
+++ src/analysis_311.py
@@ -8,15 +8,17 @@ neighborhood = 'Jamaica Plain'
 department = 'Public Works Department'
 
 # Create a query for Jamaica Plain
-back_bay_parking = Data_Pull(url,
-                      num_records,
-                      case_status,
-                      department,
-                      neighborhood)
+jp = Data_Pull(url,
+               num_records,
+               case_status,
+               department,
+               neighborhood)
 
 # Get all the data for the Jamaica Plain
 jp.list_case_types()
+
 df = jp.return_data()
-df
+
+df = jp.calculate_diff(df)
 # Write data to a csv
 df.to_csv('jp.csv', index = False)
